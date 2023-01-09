6ab7415e1ac5876cc0a2fa77e02b30bcb06da56b
mehedi hasan <{ID}+{username}@users.noreply.github.com>
2023-01-09 12:55:28 +0600

    login validation

---
+++ user/login.php
@@ -0,0 +1,24 @@
+<?php
+include "../connection.php";
+
+$userEmail = $_POST['user_email'];
+$userPassword = md5($_POST['user_password']);
+
+$sqlQuery = "SELECT * FROM users_table WHERE 
+user_email='$userEmail', AND
+user_password='$userPassword' ";
+
+$resultOfQuery = $connectNow->query($sqlQuery);
+
+if ($resultOfQuery->num_rows > 0) {
+    $userRecord = array();
+    while ($rowfound = $resultOfQuery->fetch_assoc()) {
+        $userRecord = $rowfound;
+    }
+    echo json_encode(array(
+        "success" => true,
+        "userData" => $userRecord[0],
+    ));
+} else {
+    echo json_encode(array("success" => false));
+}
--- user/signup.php
+++ user/signup.php
@@ -1,20 +1,17 @@
 <?php
 include "../connection.php";
 // post
-$userName=$_POST['user_name'];
-$userEmail=$_POST['user_email'] ;
+$userName = $_POST['user_name'];
+$userEmail = $_POST['user_email'];
 $userPassword = md5($_POST['user_password']);
 
-$sqlQuery="INSERT INTO users_table SET 
+$sqlQuery = "INSERT INTO users_table SET 
 user_name='$userName',
 user_email='$userEmail',
 user_password='$userPassword'";
-$resultOfQuery=$connectNow->query($sqlQuery);
-if($resultOfQuery){
-    echo json_encode(array("success"=>true));
-}
-else{
+$resultOfQuery = $connectNow->query($sqlQuery);
+if ($resultOfQuery) {
+    echo json_encode(array("success" => true));
+} else {
     echo json_encode(array("success" => false));
-    
 }
-// get 
